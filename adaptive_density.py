d9d97bc8f65bbb6c52665735146812546ecb8361
ChrisPGraphics <christopherpalazzolo@cmail.carleton.ca>
2025-08-13 21:03:52 -0400

    Added command args for adaptive density

--- adaptive_density.py
+++ adaptive_density.py
@@ -2,19 +2,19 @@ import os.path
 import random
 import typing
 
+import argparse
 import bridson
-import matplotlib.pyplot as plt
 import numpy as np
 from scipy.spatial import cKDTree
 
-import common
 import analysis
-import synthesis
+import common
 import hierarchy_node
+import synthesis
 
 
 def adaptive_primary_textons(textons: hierarchy_node.VectorNode, texton_library: analysis.result_objects.PrimaryTextonResult, position_multiplier: float):
-    textons.exterior *= multiplier
+    textons.exterior *= position_multiplier
 
     initial_textons = [texton.copy(deep_copy=True) for texton in textons.children]
     new_size = (int(textons.get_bounding_width()), int(textons.get_bounding_width()))
@@ -45,7 +45,7 @@ def adaptive_secondary_textons(
         textons: hierarchy_node.VectorNode, texton_library: analysis.result_objects.SecondaryTextonResult, position_multiplier: float
 ) -> hierarchy_node.VectorNode:
 
-    textons.exterior *= multiplier
+    textons.exterior *= position_multiplier
     new_size = (int(textons.get_bounding_width()), int(textons.get_bounding_width()))
 
     for child in textons.children:
@@ -93,28 +93,48 @@ def adaptive_gradient_field(
 
 if __name__ == '__main__':
     common.logger.configure_logger()
-    exemplar = "rust_1"
 
-    multiplier = 2
+    parser = argparse.ArgumentParser(
+        description="Increases the spacing between textons of a result and then fills in the gaps with new textons to match the exemplar density.",
+        epilog='If there are any issues or questions, feel free to visit our GitHub repository at '
+               'https://github.com/ChrisPGraphics/ByExampleSynthesisOfVectorTextures'
+    )
 
-    analysis_path = os.path.join("intermediate", exemplar)
-    result_path = os.path.join("output", exemplar)
+    parser.add_argument(
+        'image_name',
+        help="The name of the processed file. "
+             "Note that this is not the path to the image nor the directory in the intermediate folder. "
+             "Just the base name of the image without file extension. "
+             "For example, if the image path is /foo/bar.png and you analyzed the image with "
+             "'python analyze_texture.py /foo/bar.png', "
+             "you would call this script with 'python synthesize_texture.py bar'"
+    )
+
+    parser.add_argument(
+        '--multiplier', default=2, type=float,
+        help="The multiplier to move textons by. For example, a value of 2 doubles the space between textons."
+    )
+
+    args = parser.parse_args()
+
+    analysis_path = os.path.join("intermediate", args.exemplar)
+    result_path = os.path.join("output", args.exemplar)
 
     primary_textons = hierarchy_node.VectorNode.load(os.path.join(result_path, "primary_textons.dat"))
     initial_primary_textons = analysis.result_objects.PrimaryTextonResult.load(os.path.join(analysis_path, "primary_textons.dat"))
     initial_size = primary_textons.get_bounding_size()
 
-    primary_textons = adaptive_primary_textons(primary_textons, initial_primary_textons, multiplier)
+    primary_textons = adaptive_primary_textons(primary_textons, initial_primary_textons, args.multiplier)
 
     secondary_textons = hierarchy_node.VectorNode.load(os.path.join(result_path, "secondary_textons.dat"))
     initial_secondary_textons = analysis.result_objects.SecondaryTextonResult.load(os.path.join(analysis_path, "secondary_textons.dat"))
 
-    secondary_textons = adaptive_secondary_textons(secondary_textons, initial_secondary_textons, multiplier)
+    secondary_textons = adaptive_secondary_textons(secondary_textons, initial_secondary_textons, args.multiplier)
 
     gradient_field = analysis.result_objects.GradientFieldResult.load(os.path.join(result_path, "gradient_field.dat"))
     initial_gradient_field = analysis.result_objects.GradientFieldResult.load(os.path.join(analysis_path, "gradient_field.dat"))
 
-    gradient_field, gradient_field_raster = adaptive_gradient_field(gradient_field, initial_gradient_field, multiplier, initial_size)
+    gradient_field, gradient_field_raster = adaptive_gradient_field(gradient_field, initial_gradient_field, args.multiplier, initial_size)
 
     synthesis.export_result(
         primary_textons, secondary_textons, gradient_field, gradient_field_raster, os.path.join("output", "adaptive_density")
